394eda67477d1342063d59e689e2fafa47d356f7
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-21 11:22:08 -0300

    Added first assets and texts to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import factImage from "../images/fact.svg"
+
+const Fact = ({text, number}) => (
+  <div>
+    <img src={factImage} alt="Fact" />
+    <h4>Fact #{number}</h4>
+    <p>{text}</p>
+  </div>
+)
+
+const Input = ({text, icon}) => (
+  <>
+    <img src={icon} alt="Icon"></img>
+    <input placeholder={text}></input>
+  </>
+)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h3>jifcam</h3>
+    <h1><strong>GET MORE</strong> Views</h1>
+    <h1><strong>COLLAB</strong> with other creators</h1>
+    <h1><strong>SHOWCASE</strong> your talent</h1>
+    <h2>with<br/> Mini Vertical Videos on Jifcam</h2>
+    <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
+    <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
+    <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
+    <Input text={"E-mail"} />
+    <Input text={"Password"} />
+    <button>Get Started</button>
   </Layout>
 )
 
