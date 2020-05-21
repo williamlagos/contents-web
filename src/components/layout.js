a7305d6e73c16e7fa5b6d2e9433935ca515cfb65
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-21 12:24:12 -0300

    Added the last items and started to position correctly

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,20 +7,17 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { Grommet, Box, Image, Stack, grommet } from "grommet"
+import { Grommet, Box, grommet } from "grommet"
 
-import { mockupImage } from "../images/mockup.png"
-import { bgImage } from "../images/bg.png"
+import bgImage from "../images/bg.png"
 // import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <Grommet theme={grommet}>
-      <Stack anchor="right">
-        <Image src={bgImage} />
-        <Image src={mockupImage} />
-      </Stack>
-      <Box>{children}</Box>
+      <Box background={`url(${bgImage})`}>
+        {children}
+      </Box>
     </Grommet>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import SEO from "../components/seo"
 import factImage from "../images/fact.svg"
 import emailImage from "../images/email.svg"
 import passwordImage from "../images/password.svg"
+import mockupImage from "../images/mockup.png"
 
 const Fact = ({text, number}) => (
   <Box direction="column">
@@ -19,24 +20,29 @@ const Fact = ({text, number}) => (
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Box>
-      <Heading level={3}>jifcam</Heading>
-      <Heading><strong>GET MORE</strong> Views</Heading>
-      <Heading><strong>COLLAB</strong> with other creators</Heading>
-      <Heading><strong>SHOWCASE</strong> your talent</Heading>
-      <Heading level={2}>with<br/> Mini Vertical Videos on Jifcam</Heading>
-    </Box>
-    <Box>
-      <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
-      <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
-      <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
-    </Box>
-    <Box>
-      <Box direction="row">
-        <TextInput icon={<Image src={emailImage}/>} placeholder="E-mail" />
-        <TextInput icon={<Image src={passwordImage}/>} placeholder="Password" />
+    <Box direction="row">
+      <Box>
+        <Box>
+          <Heading level={3}>jifcam</Heading>
+          <Heading margin="none"><strong>GET MORE</strong> Views</Heading>
+          <Heading margin="none"><strong>COLLAB</strong> with other creators</Heading>
+          <Heading margin="none"><strong>SHOWCASE</strong> your talent</Heading>
+          <Heading level={2}>with<br/> Mini Vertical Videos on Jifcam</Heading>
+        </Box>
+        <Box direction="row">
+          <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
+          <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
+          <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
+        </Box>
+        <Box>
+          <Box direction="row">
+            <TextInput icon={<Image src={emailImage}/>} placeholder="E-mail" />
+            <TextInput icon={<Image src={passwordImage}/>} placeholder="Password" />
+          </Box>
+          <Button primary label="Get Started" />
+        </Box>
       </Box>
-      <Button primary label="Get Started" />
+      <Box><Image fit="contain" src={mockupImage} /></Box>
     </Box>
   </Layout>
 )
