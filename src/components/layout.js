2a76f117bf5840f6346095bc43f7e170a7a2d62b
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-21 11:38:29 -0300

    Commented out default header, footer and styles and started to use Grommet

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,11 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import { Grommet } from "grommet"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,8 +25,8 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <Grommet>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div
         style={{
           margin: `0 auto`,
@@ -34,13 +35,13 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        {/*<footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer>*/}
       </div>
-    </>
+    </Grommet>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Box, MaskedInput, Image, Text, Heading, Button } from "grommet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,34 +7,40 @@ import SEO from "../components/seo"
 import factImage from "../images/fact.svg"
 
 const Fact = ({text, number}) => (
-  <div>
-    <img src={factImage} alt="Fact" />
-    <h4>Fact #{number}</h4>
-    <p>{text}</p>
-  </div>
+  <>
+    <Image src={factImage} alt="Fact" />
+    <Heading level={4}>Fact #{number}</Heading>
+    <Text>{text}</Text>
+  </>
 )
 
-const Input = ({text, icon}) => (
+const BigInput = ({text, icon}) => (
   <>
-    <img src={icon} alt="Icon"></img>
-    <input placeholder={text}></input>
+    <Image src={icon} alt="Icon"></Image>
+    <MaskedInput placeholder={text}></MaskedInput>
   </>
 )
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h3>jifcam</h3>
-    <h1><strong>GET MORE</strong> Views</h1>
-    <h1><strong>COLLAB</strong> with other creators</h1>
-    <h1><strong>SHOWCASE</strong> your talent</h1>
-    <h2>with<br/> Mini Vertical Videos on Jifcam</h2>
-    <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
-    <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
-    <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
-    <Input text={"E-mail"} />
-    <Input text={"Password"} />
-    <button>Get Started</button>
+    <Box>
+      <Heading level={3}>jifcam</Heading>
+      <Heading><strong>GET MORE</strong> Views</Heading>
+      <Heading><strong>COLLAB</strong> with other creators</Heading>
+      <Heading><strong>SHOWCASE</strong> your talent</Heading>
+      <Heading level={2}>with<br/> Mini Vertical Videos on Jifcam</Heading>
+    </Box>
+    <Box>
+      <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
+      <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
+      <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
+    </Box>
+    <Box>
+      <BigInput text={"E-mail"} />
+      <BigInput text={"Password"} />
+      <Button>Get Started</Button>
+    </Box>
   </Layout>
 )
 
