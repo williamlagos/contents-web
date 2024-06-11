786123d8d55e651c0cf743dcfd901dad74dfa6d1
William Oliveira de Lagos <william.lagos@icloud.com>
2024-06-11 21:11:03 +0200

    feat: added main grommet component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Grommet } from 'grommet'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Grommet full>
+      <App />
+    </Grommet>
+  </React.StrictMode>
 )
