f639d1b25ad0b7be48951a905bfb854056109725
William Oliveira de Lagos <william.lagos@icloud.com>
2024-09-18 12:08:47 +0200

    feat: moved grommet object to layout template

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,14 +1,21 @@
-import { Main } from 'grommet'
+import { Grommet, Main } from 'grommet'
+
 import { Footer } from '../../molecules'
 import { Navbar } from '../../organisms'
 
-const Layout = ({ children }: any) => [
-  <Navbar
-    session={false}
-    pages={[{ label: 'Facebook' }, { label: 'Twitter' }, { label: 'YouTube' }]}
-  />,
-  <Main background="#eee">{children}</Main>,
-  <Footer />
-]
+const Layout = ({ children }: any) => (
+  <Grommet>
+    <Navbar
+      session={false}
+      pages={[
+        { label: 'Facebook' },
+        { label: 'Twitter' },
+        { label: 'YouTube' }
+      ]}
+    />
+    <Main background="#eee">{children}</Main>
+    <Footer />
+  </Grommet>
+)
 
 export default Layout
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Grommet } from 'grommet'
 
 import { BlogPage, MainPage, NotFoundPage } from './components/pages'
 import './index.css'
@@ -10,19 +9,17 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
-    errorElement: <NotFoundPage />,
+    errorElement: <NotFoundPage />
   },
   {
     path: '/blog',
     element: <BlogPage />,
-    errorElement: <NotFoundPage />,
-  },
+    errorElement: <NotFoundPage />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Grommet full>
-      <RouterProvider router={router} />
-    </Grommet>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
